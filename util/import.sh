76bc40efa2316a838eae082f30a8bd55d8e82531
Martin Czuchra <martin.czuchra@googlemail.com>
2010-12-04 20:37:50 +0100

    emits usage if no url given.

--- util/import.sh
+++ util/import.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 if [ -d ../data ]; then
-  for f in ../data/*Data*
-  do
-    echo "Importing $f ..."
-    ./tbm.rb import $f $1
-  done
+
+  if [ "$1" != "" ]; then
+    for f in ../data/*Data*
+    do
+      echo "Importing $f ..."
+      ./tbm.rb import $f $1
+    done
+  else
+    echo "Usage: $0 URL"
+  fi
+  
 else
   echo There is no ../data folder. Add data and rerun $0.
 fi
