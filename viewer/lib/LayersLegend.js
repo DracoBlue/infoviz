d2cf15f81167c4586b2a1b3caa1b46105d769033
DracoBlue <JanS@DracoBlue.de>
2011-02-03 23:01:32 +0100

    added LayersLegend.js

---
+++ viewer/lib/LayersLegend.js
@@ -0,0 +1,53 @@
+project.LayersLegend = function()
+{
+    /*
+     * Must be kept empty, because of the last line of this file:
+     * project.LayersLegend = new
+     * Class(project.LayersLegend.prototype);
+     * 
+     * Do initialization stuff in #initialize!
+     */
+};
+
+project.LayersLegend.prototype = {
+
+    Implements: [
+        Options, Events
+    ],
+
+    options: {
+    },
+
+    database: null,
+
+    dom_element: null,
+
+    initialize: function(database, options)
+    {
+        var self = this;
+        this.database = database;
+        this.setOptions(options);
+
+        this.dom_element = new Element('ul', {
+            'class': 'layers_legend'
+        });
+
+        this.database.retrieveLayers(function(layers)
+        {
+            layers.each(function(layer)
+            {
+                self.dom_element.grab(new Element('li', {
+                    'text': layer.name,
+                    'style': 'color: #' + layer.color
+                }));
+            });
+        });
+    },
+
+    toElement: function()
+    {
+        return this.dom_element;
+    }
+};
+
+project.LayersLegend = new Class(project.LayersLegend.prototype);
