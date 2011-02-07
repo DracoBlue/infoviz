dba8e157e9957a0e291c7ed9bba1722687492793
Richard Metzler <richard@eaglefeed.me>
2011-02-07 12:36:09 +0100

    legendenbilder kleiner

--- util/legende.sh
+++ util/legende.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-coffee schichten.coffee legende1.png 100 100 939393
-coffee schichten.coffee legende2.png 100 100 E3C24D
-coffee schichten.coffee legende3.png 100 100 C08829
-coffee schichten.coffee legende4.png 100 100 778FBA
-coffee schichten.coffee legende5.png 100 100 6CA193
+SIZE=20
+coffee schichten.coffee legende1.png $SIZE $SIZE 939393
+coffee schichten.coffee legende2.png $SIZE $SIZE E3C24D
+coffee schichten.coffee legende3.png $SIZE $SIZE C08829
+coffee schichten.coffee legende4.png $SIZE $SIZE 778FBA
+coffee schichten.coffee legende5.png $SIZE $SIZE 6CA193
